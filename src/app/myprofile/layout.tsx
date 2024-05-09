3f2939fae47563a2cf142d3cec9abdca782f32d0
Anand Mohan <anand18mohan@gmail.com>
2024-05-09 21:02:00 +0530

    added dockerfile and gitignore

--- src/app/myprofile/layout.tsx
+++ src/app/myprofile/layout.tsx
@@ -1,33 +1,31 @@
 import Navbar from "@/components/Navbar";
 import Profile from "@/components/Profile";
 import { Spotlight } from "@/components/ui/Spotlight";
-import {Button} from "@/components/ui/moving-border"
+import { Button } from "@/components/ui/moving-border";
 import Link from "next/link";
 export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-        <>
-         <Spotlight
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
       <Navbar />
-      <div className="mt-[130px] ml-8">
-
+      <div className="justify-center mt-[130px] ml-8">
         <Link href={"/myprofile"} className="mt-[150px]">
-                        <Button
-                        borderRadius="1.75rem"
-                        className=" bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
-                        >
-                        Edit Profile
-                        </Button>
-            </Link>
-
+          <Button
+            borderRadius="1.75rem"
+            className=" bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+          >
+            Edit Profile
+          </Button>
+        </Link>
       </div>
-      < Profile />
-        </>
-    );
-  }
+      <Profile />
+    </>
+  );
+}
